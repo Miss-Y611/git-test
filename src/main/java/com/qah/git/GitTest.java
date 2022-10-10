01b84717e1145e2f591703ca355de3a4ae00507f
Miss <123.com>
2022-10-10 14:30:36 +0800

    first commit

---
+++ src/main/java/com/qah/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.qah.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author Miss
+ * @Date 2022/10/10 14:28
+ * Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
